7fb251146e248563775c1b9d98438fadbb92ddad
rico <r03292760@gmail.com>
2026-08-23 11:24:12 +0200

    feat: complete banking and ATM system

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Rico Scripts'
+description 'RS Banking with bank branches, secure PIN terminals and ATM transactions'
+version '1.0.0'
+
+ui_page 'web/index.html'
+
+shared_scripts { '@ox_lib/init.lua', 'config.lua' }
+client_script 'client/main.lua'
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server/main.lua' }
+
+files { 'web/index.html', 'web/style.css', 'web/app.js' }
+
+dependencies { 'es_extended', 'ox_lib', 'oxmysql', 'ox_target', 'rs-economie' }
